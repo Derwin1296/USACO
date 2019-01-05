edcf9c7117b4aa5fab283687c5ab1fff1b51a4b4
Derwin1296 <34933140+Derwin1296@users.noreply.github.com>
2019-01-05 13:34:50 -0700

    Update Dijkstra.cpp

--- Graphs/Dijkstra.cpp
+++ Graphs/Dijkstra.cpp
@@ -1,1 +1,25 @@
-for(
+for(int i = 1; i <= n; i++) d[i] = INF;
+d[root] = 0;
+
+void dijkstra(int root){
+  std :: priority_queue<pi> q;
+  q.push({root, 0});
+  
+  while(!q.empty()){
+    pi cPair = q.front();
+    q.pop();
+    
+    int v = cPair.f; int dist = cPair.s;
+    if(vis[v]) continue;
+    vis[v] = true;
+    
+    vector<pi> n = adjList[v];
+    for(auto p : n){
+        int vertex = p.f; int weight = p.s;
+        if(d[v] + weight < d[vertex]){
+            d[vertex] = d[v] + weight;
+            q.push({-d[vertex], vertex);
+        }
+    }
+  }
+}
