a342e47df66c935df8b7c48aac6f9e4aac57fadb
Derwin1296 <34933140+Derwin1296@users.noreply.github.com>
2019-01-06 17:40:44 -0700

    Update P3.cpp
    Flood Fill + Binary Search

--- 2014 January Silver/P3.cpp
+++ 2014 January Silver/P3.cpp
@@ -1,41 +1,116 @@
 #include <iostream>
-#include <vector>
-#include <cstdio>
-
+#include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+
+const int MOD = 1000000007;
+const ll INF = 1e18;
+const int MX = 100001;
+ll grid[510][510];
+bool waypoint[510][510];
+pi entryPoint;
+bool vis[510][510];
+
+int xDir[4] = {1, -1, 0, 0};
+int yDir[4] = {0, 0, -1, 1};
+// in bounds
+bool inBounds(int x, int y, int m, int n){
+    return x >= 0 && x < m && y >= 0 && y < n;
+}
+// bfs
+void bfs(pi point, int m, int n, int d){
+    int xCoor = point.f; int yCoor = point.s;
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            vis[i][j] = false;
+        }
+    }
+    queue<pi> q;
+    q.push({xCoor, yCoor});
+    while(!q.empty()){
+        pi cPair = q.front();
+        q.pop();
+        int curX = cPair.f; int curY = cPair.s;
+        
+        vis[curX][curY] = true;
+        for(int i = 0; i < 4; i++){
+            int newX = curX + xDir[i];
+            int newY = curY + yDir[i];
+            if(inBounds(newX, newY, m, n) && !vis[newX][newY] && abs(grid[newX][newY] - grid[curX][curY]) <= d){
+                vis[newX][newY] = true;
+                q.push({newX, newY});
+            }
+        }
+    }
+}
+// waypoints all true
+bool allWayPoints(int m, int n, vector<pi> toCheck){
+    for(int i = 0; i < toCheck.size(); i++){
+        pi cPair = toCheck[i];
+        if(!vis[cPair.f][cPair.s]){
+            return false;
+        }
+    }
+    return true;
+}
 int main() {
-  freopen("superbull.in", "r", stdin);
-  freopen("superbull.out", "w", stdout);
-
-  int N;
-  cin >> N;
-  vector<int> A(N);
-  for (int i = 0; i < N; i++) {
-    cin >> A[i];
-  }
-
-  long long result = 0;
-  vector<bool> used(N, false);
-  vector<int> D(N, 0);
-  for (int i = 0; i < N; i++) {
-    /* Find the index, j, of the 'furthest' node from the current spanning tree. */
-    int j = -1;
-    for (int k = 0; k < N; k++) {
-      if (used[k]) continue;
-      if (j == -1 || D[k] > D[j]) {
-        j = k;
-      }
+    vector<pi> wayPoints;
+    int M, N; cin >> M >> N;
+    F0R(i, M){
+        F0R(j, N){
+            cin >> grid[i][j];
+        }
     }
-
-    /* Update the result and 'relax' the edges out of vertex j. */
-    result += D[j];
-    used[j] = true;
-    for (int k = 0; k < N; k++) {
-      D[k] = max(D[k], A[j] ^ A[k]);
+    F0R(i, M){
+        F0R(j, N){
+            int x; cin >> x;
+            if(x == 1){
+                waypoint[i][j] = true;
+                entryPoint = {i, j};
+                wayPoints.pb({i, j});
+            }
+        }
     }
-  }
-
-  cout << result << endl;
-  return 0;
+    ll low = 0; ll high = 1e9;
+    while(low < high){
+        ll mid = (low + high) / 2;
+        bfs(entryPoint, M, N, mid);
+        bool valid = allWayPoints(M, N, wayPoints);
+        if(valid){
+            high = mid;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    cout << low;
 }
