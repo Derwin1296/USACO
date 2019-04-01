a23593c9eae243e503ab18f6f538e8a153a017f9
Derwin1296 <34933140+Derwin1296@users.noreply.github.com>
2019-04-01 08:27:54 -0600

    Create Subsets.cpp

---
+++ Math/Subsets.cpp
@@ -0,0 +1,15 @@
+// Generate all possible subsets of a set
+vector<vector<int>> genSub(vector<int> list){
+    vector<vector<int>> fSet;
+    // All #s up to 2^(# elements in the set)
+    for(int i = 0; i < pow(2, list.size()); i++){
+        vector<int> subset;
+        for(int j = 0; (1 << j) <= i; j++){
+            if(i & (1 << j)){
+                subset.pb(list[j]);
+            }
+        }
+        fSet.pb(subset);
+    }
+    return fSet;
+}
