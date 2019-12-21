05868693787bafe89e83f623ac38dc9abe9aff68
Derwin1296 <34933140+Derwin1296@users.noreply.github.com>
2019-12-21 10:19:39 -0800

    Update KMP.cpp

--- String/KMP.cpp
+++ String/KMP.cpp
@@ -1,3 +1,31 @@
+void compute_lps(int M, int *lps, string pat){
+    int len = 0; int i = 1;
+    lps[0] = 0;
+    while(i < M){
+        if(pat[i] == pat[len]){
+            len ++;
+            lps[i] = len;
+            i ++;
+        }
+        else{
+            if(len != 0){
+                len = lps[len - 1];
+            }
+            else{
+                lps[i] = 0;
+                i ++;
+            }
+        }
+    }
+}
+/** BAAA
+    AAA
+    i = 0, j = 0: i = 1, j = 0
+    i = 1, j = 0: i = 2, j = 1
+    i = 2, j = 1: i = 3: j = 2
+    i = 3, j = 2: i = 4:, j = 3:
+**/
+
 void KMP(string txt, string pat){
     int N = txt.size(); int M = pat.size();
     int lps[M];
@@ -24,4 +52,12 @@ void KMP(string txt, string pat){
             }
         }
     }
+}
+int main()
+{
+    string str = "AABAACAADAABAABA";
+    string pat = "AABA";
+    // string str = "ABABDABACDABABCABAB";
+    // string pat = "ABABCABAB";
+    KMP(str, pat);
 }
