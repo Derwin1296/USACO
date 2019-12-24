2e3456be56a4e8e7bb4f9e25a9c8f655a0b68689
Derwin1296 <34933140+Derwin1296@users.noreply.github.com>
2019-12-24 13:48:07 -0700

    Create Segment Tree.cpp

---
+++ Tree/Segment Tree.cpp
@@ -0,0 +1,18 @@
+void build(){
+    for(int i = n - 1; i >= 0; i--){
+        tree[i] = tree[i << 1] + tree[i << 1 | 1];
+    }
+}  
+
+void modify(int p, int value){
+    for(tree[p += n] = value; p > 1; p >>= 1)
+        tree[p >> 1] = tree[p] + tree[p ^ 1];
+}
+int query(int l, int r) {  // sum on interval [l, r)
+    int res = 0;
+    for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+        if (l&1) res += tree[l++];
+        if (r&1) res += tree[--r];
+    }
+    return res;
+}
