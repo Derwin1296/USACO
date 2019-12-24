663382ab4eba230d4e414fd01ec25718d1f83e85
Derwin1296 <34933140+Derwin1296@users.noreply.github.com>
2019-12-24 13:50:02 -0700

    Update Segment Tree.cpp

--- Tree/Segment Tree.cpp
+++ Tree/Segment Tree.cpp
@@ -8,6 +8,7 @@ void modify(int p, int value){
     for(tree[p += n] = value; p > 1; p >>= 1)
         tree[p >> 1] = tree[p] + tree[p ^ 1];
 }
+
 int query(int l, int r) {  // sum on interval [l, r)
     int res = 0;
     for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
